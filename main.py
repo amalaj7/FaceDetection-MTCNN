ac1e3216ad372421174f88435b24961a3bfdf32c
Amal <amalsaga@gmail.com>
2021-06-14 11:39:01 +0530

    main files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from mtcnn.mtcnn import MTCNN
 import cv2
 from utils import utils
 
-ben_image = cv2.imread('ben1.jpg')
+ben_image = cv2.imread('Ben_affleck/ben1.jpg')
 #cv2.imshow("mycastle", ben_image)
 
 detector = MTCNN()
@@ -15,6 +15,6 @@ marked_image = utils.create_bbox(ben_image)
 filename = "saved_detections/ben_affleck_detection.jpg"
 cv2.imwrite(filename, marked_image)
 
-marked_image1 = utils.create_bbox(cv2.imread('robert1.webp'))
+marked_image1 = utils.create_bbox(cv2.imread('Robert_jr/robert1.webp'))
 filename = "saved_detections/robert_detection.jpg"
 cv2.imwrite(filename, marked_image1)
