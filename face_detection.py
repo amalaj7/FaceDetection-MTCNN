6625a174af8b495576416fc8d6c419b1d8fbbecf
Amal <amalsaga@gmail.com>
2021-06-14 13:29:53 +0530

    face detect

--- face_detection.py
+++ face_detection.py
@@ -2,8 +2,8 @@ from mtcnn.mtcnn import MTCNN
 import cv2
 from utils import utils
 
-ben_image = cv2.imread('Ben_affleck/ben1.jpg')
-#cv2.imshow("mycastle", ben_image)
+ben_image = cv2.imread('Robert_jr/robert1.webp')
+#cv2.imshow("benImage", ben_image)
 
 detector = MTCNN()
 
@@ -12,9 +12,5 @@ for face in faces:
     print(face)
 
 marked_image = utils.create_bbox(ben_image)
-filename = "saved_detections/ben_affleck_detection.jpg"
-cv2.imwrite(filename, marked_image)
-
-marked_image1 = utils.create_bbox(cv2.imread('Robert_jr/robert1.webp'))
 filename = "saved_detections/robert_detection.jpg"
-cv2.imwrite(filename, marked_image1)
+cv2.imwrite(filename, marked_image)
