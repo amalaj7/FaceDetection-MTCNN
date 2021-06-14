87b35f7d422ce2c2f39a702635edd716f11a01fe
Amal <amalsaga@gmail.com>
2021-06-14 13:07:41 +0530

    face verification added

---
+++ face_verification.py
@@ -0,0 +1,27 @@
+from mtcnn.mtcnn import MTCNN
+import cv2 as cv2
+import numpy as np
+from keras_vggface.utils import preprocess_input
+from keras_vggface.vggface import VGGFace
+from scipy.spatial.distance import cosine
+from utils import utils
+
+ben_image = cv2.imread('Ben_affleck/ben1.jpg')
+
+detector = MTCNN()
+
+faces = detector.detect_faces(ben_image)
+for face in faces:
+    print(face)
+
+#marked_image = utils.create_bbox(ben_image)
+
+extract_face = utils.extractFace('Ben_affleck/ben1.jpg')
+filename = "Extracted_FaceImage/ben_cropped.jpg"
+cv2.imwrite(filename, extract_face)
+
+# To extract the face and get the embeddings
+extractedFaces = [utils.extractFace(image) for image in ['Ben_affleck/ben1.jpg','Robert_jr/robert1.webp']]
+
+# To compare the faces and get the similarity score 
+print(utils.getSimilarity(extractedFaces))
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,9 @@
 import cv2
 from mtcnn.mtcnn import MTCNN
+import numpy as np
+from keras_vggface.utils import preprocess_input
+from keras_vggface.vggface import VGGFace
+from scipy.spatial.distance import cosine
 
 def create_bbox(image):
     detector = MTCNN()
@@ -20,4 +24,42 @@ def create_bbox(image):
     cv2.circle(image,(keypoints['mouth_right']), 2, (0,155,255), 2)
 
     return image
+
+def extractFace(image,resize=(224,224)):
+    image = cv2.imread(image)
+    detector = MTCNN()
+    faces = detector.detect_faces(image)
+    x1,y1,width,height = faces[0]['box']
+    x2,y2 = x1 + width , y1 + height
+
+    face_boundary = image[y1:y2,x1:x2] # contains the facial objects  
+    face_image = cv2.resize(face_boundary,resize)
+    return face_image
+
+def getEmbedding(faces):
+    '''
+    This function get the faces and return the embeddings vector
+    from the faces using VGGFace which is pretrained model that is being trained on 3.3 millions faces.
+
+    '''
+
+    face = np.asarray(faces,"float32")
+    face = preprocess_input(face,version = 2)
+
+    model = VGGFace(model = 'resnet50',include_top=False,input_shape=(224,224,3), pooling='avg')
+
+    return model.predict(face)
+
+def getSimilarity(faces):
+    embeddings = getEmbedding(faces)
+
+    score = cosine(embeddings[0],embeddings[1])
+
+    if score <=0.5 :
+        return "Face Matched", score
+    else:
+        return "Face Doesnt Match", score
+
+
+
 
